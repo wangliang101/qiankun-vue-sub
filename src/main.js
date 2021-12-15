40904a1252d91412560872959903e4e996c3c5ae
wliang <wliang@infervision.com>
2021-12-16 01:19:16 +0800

    [add] 生命周期函数暴露，webpack配置修改

--- src/main.js
+++ src/main.js
@@ -1,8 +1,42 @@
 import { createApp } from 'vue'
 import App from './App.vue';
-import router from './router';
-const app = createApp(App);
+import route from './router';
 
-app.use(router);
-app.mount('#app');
+
+let router = null;
+let instance = null;
+
+
+function render(props = {}) {
+  const { container } = props;
+  router = route;
+
+
+  instance = createApp(App);
+  instance.use(router);
+  instance.mount(container ? container.querySelector('#app') : '#app');
+}
+
+
+// 独立运行时
+if (!window.__POWERED_BY_QIANKUN__) {
+  render();
+}
+
+
+export async function bootstrap() {
+  console.log('[vue] vue app bootstraped');
+}
+export async function mount(props) {
+  console.log('[vue] props from main framework', props);
+  render(props);
+}
+export async function unmount() {
+  console.log('instance', instance)
+  instance.unmount();
+  instance._container.innerHTML = '';
+  instance = null;
+  router = null;
+}
+
 
